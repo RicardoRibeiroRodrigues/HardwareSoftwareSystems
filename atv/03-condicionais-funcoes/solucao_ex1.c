30c3a69d3f9d56c42c3b238fa5c0644c5fbac2d3
RicardoRibeiroRodrigues <riywar@gmail.com>
2022-09-08 17:28:22 -0300

    Ex1 e 2 feitos

--- atv/03-condicionais-funcoes/solucao_ex1.c
+++ atv/03-condicionais-funcoes/solucao_ex1.c
@@ -1,3 +1,3 @@
-/* Implementar função solucao_ex1 */
-
-
+int solucao_ex1(long a, long b, long c) {
+    return (a + b + (4 * c)) >= (c + a * a + (2 * b));
+}
--- atv/03-condicionais-funcoes/solucao_ex2.c
+++ atv/03-condicionais-funcoes/solucao_ex2.c
@@ -1,3 +1,10 @@
 long vezes2(long a);
 
 /* Implementar função solucao_ex2 */
+long solucao_ex2(long a, long b) {
+    long res = vezes2(b);
+    if (a >= res) {
+        return a + res;
+    }
+    return a + a + res;
+}
