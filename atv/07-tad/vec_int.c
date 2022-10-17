524415c61f065516b754e63ea011982a888a7df0
RicardoRibeiroRodrigues <riywar@gmail.com>
2022-10-17 14:31:24 -0300

    Termina atv 7

--- atv/07-tad/vec_int.c
+++ atv/07-tad/vec_int.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-
 #include "vec_int.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _vec_int {
     int *data;
@@ -8,27 +8,68 @@ typedef struct _vec_int {
     int capacity;
 } vec_int;
 
-
 vec_int *vec_int_create() {
-    return NULL;
+    vec_int *new_vec = malloc(sizeof(vec_int));
+    new_vec->capacity = 2;
+    new_vec->data = malloc(sizeof(int) * new_vec->capacity);
+    new_vec->size = 0;
+    return new_vec;
 }
 
 void vec_int_destroy(vec_int **_v) {
-
+    free((*_v)->data);
+    free(*_v);
+    *_v = NULL;
 }
 
 int vec_int_size(vec_int *v) {
-    return 0;
+    return v->size;
 }
 
 int vec_int_at(vec_int *v, int pos, int *val) {
+    if (pos < vec_int_size(v)) {
+        *val = v->data[pos];
+        return 1;
+    }
     return 0;
 }
 
 int vec_int_insert(vec_int *v, int pos, int val) {
+    if (v->size + 1 > v->capacity) {
+        v->capacity = v->capacity * 2;
+        v->data = realloc(v->data, v->capacity * sizeof(int));
+        if (v->data == NULL)
+            return 0;
+    }
+
+    if ((pos <= vec_int_size(v)) && (pos >= 0)) {
+        v->size++;
+        int temp = v->data[pos];
+        for (int i = v->size - 1; i > pos; i--) {
+            v->data[i] = v->data[i - 1];
+        }
+        v->data[pos] = val;
+
+        return 1;
+    }
+
     return 0;
 }
 
 int vec_int_remove(vec_int *v, int pos) {
-    return 0;    
+    if (v->size - 1 < (v->capacity / 4)) {
+        v->capacity = v->capacity / 2;
+        v->data = realloc(v->data, v->capacity * sizeof(int));
+        if (v->data == NULL)
+            return 0;
+    }
+    if ((pos < vec_int_size(v)) && (pos >= 0)) {
+        for (int i = pos; i < vec_int_size(v); i++) {
+            v->data[i] = v->data[i + 1];
+        }
+        v->size--;
+        return 1;
+    }
+
+    return 0;
 }
